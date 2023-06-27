ffcccf8acd1c9f2c9062e85d7297f572294dd14d
Apoor Raj Saxena <saxenapoorv2000@gmail.com>
2023-06-27 16:56:56 +0530

    minor changes in BaseModel

--- src/main/java/com/lld/bookmyshow/models/BaseModel.java
+++ src/main/java/com/lld/bookmyshow/models/BaseModel.java
@@ -3,7 +3,11 @@ package com.lld.bookmyshow.models;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import java.util.Date;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @MappedSuperclass // Don't create a sep table, instead put its attributes to every child
 public class BaseModel {
     @Id
